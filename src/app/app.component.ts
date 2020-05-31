cd82d93152d16bf60b23ea24e576d851a40b8bc1
Lelton Borges <lelton.borges1@gmail.com>
2020-05-31 00:47:05 -0300

    Cart access: floating button and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categories', component: 'CategoriasPage' },
+      { title:  'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: '' }
     ];
 
